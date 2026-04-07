9359fa710cfcb1236d0c3d94349dada4c54db857
Saman <mohamedshaman003@gmail.com>
2026-04-07 13:12:26 +0530

    fix(webpage): remove Alpine.js script for improved performance

--- resources/views/layouts/webpage.blade.php
+++ resources/views/layouts/webpage.blade.php
@@ -433,9 +433,6 @@
 
     @livewireScripts
 
-    {{-- Alpine.js --}}
-    <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
-
     @stack('scripts')
 </body>
 </html>
